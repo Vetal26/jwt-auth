1948fd90eb56b6f1326e9a035c59f4bd897fcb8b
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-12-08 13:23:45 +0300

    login/logout

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -46,6 +46,30 @@ class UserService {
     user.isActivated = true;
     await user.save();
   }
+
+  async login(email, password) {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw ApiError.badRequest('Потльзователь с таким email не найден');
+    }
+
+    const isPassEquals = await bcrypt.compare(password, user.password);
+    if (!isPassEquals) {
+      throw ApiError.badRequest('Неверный пароль');
+    }
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
+
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken);
+
+    return token;
+  }
 }
 
 module.exports = new UserService();
